a9150744db29b2dce98d1c42a477c9335c7c91b1
joao <joao.camargo@gmail.com>
2021-05-28 17:52:41 -0300

    animation complete

---
+++ Animations/Animations/ContentView.swift
@@ -0,0 +1,55 @@
+//
+//  ContentView.swift
+//  Animations
+//
+//  Created by joao camargo on 27/05/21.
+//
+
+import SwiftUI
+
+struct CornerRotateModifier: ViewModifier {
+    let amount: Double
+    let anchor: UnitPoint
+
+    func body(content: Content) -> some View {
+        content.rotationEffect(.degrees(amount), anchor: anchor).clipped()
+    }
+}
+
+extension AnyTransition {
+    static var pivot: AnyTransition {
+        .modifier(active: CornerRotateModifier(amount: -90, anchor: .topLeading), identity: CornerRotateModifier(amount: 0, anchor: .topLeading))
+    }
+}
+
+
+struct ContentView: View {
+    
+    @State private var isShowingRed = false
+       
+    
+    var body: some View {
+        VStack {
+            Button("Tap Me") {
+                withAnimation{
+                    self.isShowingRed.toggle()
+                }
+            }
+            
+            
+                if isShowingRed {
+                    Rectangle()
+                        .fill(Color.red)
+                        .frame(width: 200, height: 200)
+                        .transition(.pivot)
+                
+            }
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
