148f4442a95997b951fcc910a14a41e26ee85643
joao <joao.camargo@gmail.com>
2021-05-30 21:35:18 -0300

    moonshot

---
+++ Moonshot/Moonshot/Austronaut.swift
@@ -0,0 +1,16 @@
+//
+//  Austronaut.swift
+//  Moonshot
+//
+//  Created by joao camargo on 30/05/21.
+//
+
+import Foundation
+
+struct Astronaut: Codable, Identifiable {
+    
+    let id: String
+    let name: String
+    let description: String 
+    
+}
