48353638c8e669295f2a7aea20bf9356ae7db808
joao <joao.camargo@gmail.com>
2021-06-06 11:24:07 -0300

    bookworm 7 de 10 complete

--- Bookworm/Bookworm/ContentView.swift
+++ Bookworm/Bookworm/ContentView.swift
@@ -8,11 +8,62 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @Environment(\.managedObjectContext) var moc
+    @FetchRequest(entity: Book.entity(), sortDescriptors: []) var books: FetchedResults<Book>
+    @State private var showingAddScreen = false
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            
+            
+            List{
+                ForEach(books, id: \.self) { book in
+                    NavigationLink(destination: DetailView(book: book)) {
+                        EmojiRatingView(rating: book.rating)
+                            .font(.largeTitle)
+                        VStack(alignment: .leading){
+                            Text(book.title ?? "Unknow title").font(.headline)
+                            Text(book.author ?? "Unknow Author").foregroundColor(.secondary)
+                        }
+                    }
+                }
+            }
+                .navigationBarTitle("Bookworm")
+                .navigationBarItems(trailing: Button(action: {
+                                                    self.showingAddScreen.toggle()})  {
+                                                            Image(systemName: "plus")
+                }).sheet(isPresented: $showingAddScreen){
+                AddBookView().environment(\.managedObjectContext, self.moc)
+            }
+        }
+        
     }
 }
+    
+    /// ways of call other view
+    //    @State var showingDetail = false
+    //
+    //    var body: some View {
+    //        NavigationView {
+    //            VStack(spacing: 20) {
+    //            Button(action: {
+    //                self.showingDetail.toggle()
+    //            }) {
+    //                Text("Show Detail")
+    //            }.sheet(isPresented: $showingDetail) {
+    //                AddBookView()
+    //            }
+    //
+    //
+    //                NavigationLink(destination: AddBookView()) {
+    //                    Text("Show Detail View")
+    //                }.navigationBarTitle("Navigation")
+    //            }
+    //        }
+    //    }
+    
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
---
+++ Bookworm/Bookworm/DetailView.swift
@@ -0,0 +1,61 @@
+//
+//  DetailView.swift
+//  Bookworm
+//
+//  Created by joao camargo on 06/06/21.
+//
+import CoreData
+import SwiftUI
+
+struct DetailView: View {
+    
+    let book: Book
+    
+    var body: some View {
+        GeometryReader { geometry in
+            VStack {
+                ZStack(alignment: .bottomTrailing){
+                    Image(self.book.genre ?? "Fantasy")
+                        .frame(maxWidth: geometry.size.width)
+                    
+                    Text(self.book.genre?.uppercased() ?? "Fantasy")
+                        .font(.caption)
+                        .fontWeight(.black)
+                        .padding(8)
+                        .foregroundColor(.white)
+                        .background(Color.black.opacity(0.75))
+                        .clipShape(Capsule())
+                        .offset(x: -5, y: -5)
+                    
+                }
+                Text(self.book.author ?? "Unknow Author").font(.title)
+                    .foregroundColor(.secondary)
+                
+                Text(self.book.review ?? "No Review").padding()
+                
+                RatingView(rating: .constant(Int(self.book.rating))).font(.largeTitle)
+                
+                Spacer()
+                
+            }
+        }.navigationBarTitle(Text(book.title ?? "Unknow book"),displayMode: .inline)
+    }
+}
+
+struct DetailView_Previews: PreviewProvider {
+    
+    static let moc = NSManagedObjectContext(concurrencyType: .mainQueueConcurrencyType)
+    
+    static var previews: some View {
+        let book = Book(context: moc)
+        book.title = "text book"
+        book.author = "Joao"
+        book.genre = "Fantasy"
+        book.rating = 4
+        book.review = "This was a great book; I really enjoyed it."
+        
+        return NavigationView {
+            DetailView(book: book)
+        }
+    }
+}
